acde1b4b202b7f6abe6f206278e0ea32bbdd585c
Paul Schreiner <paul@rokoko.com>
2022-02-10 16:37:36 +0100

    "- added some comments"

--- IK/InverseKinematics.py
+++ IK/InverseKinematics.py
@@ -7,6 +7,24 @@ from IK.Jacobian import compute_jacobian_revolute_chain, compute_jacobian_invers
 
 
 def pseudo_inverse_solver(target, p0, theta0=None, end_eff=None, tol=1.0e-4, max_iter=10, rotation_axes=None, lr=1.):
+    """ This is an IK solver using the pseudo inverse of the Jacobian to solve the optimisation problem. In case of
+    singularities (fx. gimbal lock), it will switch to using the Jacobian transpose instead. That last step is a bit
+    hacky, but it kinda works to avoid gimbal locks.
+
+    :param target: target for the optimisation problem
+    :param p0: joint offsets
+    :param theta0: initial joint angles
+    :param end_eff: end effector position with respect to its frame origin
+    :param tol: tolerance, when the error drops bellow this number, the optimisation is considered converged.
+    :param max_iter: max number of iterations before the optimisation is stopped
+    :param rotation_axes: list(str) Rotation axes of the revolute joints, currently only 'x', 'y' and 'z' are accepted.
+    Length must be equal to the length of p0. If None then all axes will be set to 'x'
+    :param lr: Learning rate
+    :return:
+    np.ndarray, computed joint angles.
+
+    TODO: project angles to -pi <-> pi interval
+    """
     if theta0 is None:
         theta0 = [0 for _ in p0]
 
--- Scripts/test.py
+++ Scripts/test.py
@@ -2,16 +2,22 @@ from IK.InverseKinematics import pseudo_inverse_solver
 import numpy as np
 
 
-'''Test with a single joint:'''
-p = np.array([[0., 1., 0.], [0., 1., 0.], [0., 0., 0.]])
+'''Test with three links:'''
+# Define the links by setting some offset parameters (the last link is the end effector)
+p = np.array([[0., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
 
-target = np.array([0., 2., 0., 1.])
-
+# Initialise the joint angles
 theta0 = np.random.uniform(0, np.pi, p.shape[0])
 print("Initial joint angles: {}".format(theta0))
 
-rotation_axes = ['x', 'y', 'z']
+# Define the rotation axes of the joint (None means all are revolute around x-axis)
+# rotation_axes = ['x', 'y', 'z']
 rotation_axes = None
+
+# Set a target
+target = np.array([0., 2., 0., 1.])
+
+# And go!
 theta = pseudo_inverse_solver(target, p, theta0=theta0, rotation_axes=rotation_axes, lr=0.5, max_iter=100)
 
 print("Computed joint angles: {}".format(theta))
